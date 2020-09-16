5e34fd5f3c4deaa4f3632e593a324a203c7bf9a7
Philip Top <phlptp@gmail.com>
2020-09-16 09:46:16 -0700

    update some of the handle types to follow naming conventions now all moving to PascalCase

--- src/helics/application_api/Federate.cpp
+++ src/helics/application_api/Federate.cpp
@@ -1190,7 +1190,7 @@ void Federate::logMessage(int level, const std::string& message) const
     }
 }
 
-Interface::Interface(Federate* federate, interface_handle id, std::string_view actName):
+Interface::Interface(Federate* federate, InterfaceHandle id, std::string_view actName):
     handle(id), mName(actName)
 {
     if (federate != nullptr) {
